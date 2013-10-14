9585d880bde38ee09a3f9c75be8d6f16146f1418
artushin <artushin@gmail.com>
2013-10-14 00:05:59 -0500

    ec2 goroot

--- scripts/reec2.sh
+++ scripts/reec2.sh
@@ -9,4 +9,5 @@ scp ~/.bashrc ubuntu@ribrak.com:~/.bashrc
 scp ~/.bash_prompt ubuntu@ribrak.com:~/.bash_prompt
 scp ~/.bash_profile ubuntu@ribrak.com:~/.bash_profile
 echo 'cd /usr/local/ribrak' | ssh ubuntu@ribrak.com "cat >> ~/.bash_profile"
+echo 'export GOROOT=/usr/local/ribrak/go"' | ssh ubuntu@ribrak.com "cat >> ~/.bash_profile"
 echo 'export PATH="$PATH:/usr/local/ribrak/go/bin"' | ssh ubuntu@ribrak.com "cat >> ~/.bash_profile"
