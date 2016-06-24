9ca9dd5fe2194878f3e591108516da60b8f0f385
artushin <artushin@gmail.com>
2016-06-24 13:02:50 -0500

    use git for sublime prefs

--- prefs/sublime/sublime.sh
+++ prefs/sublime/sublime.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-cp Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-setting
-cp Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-
-wget "https://packagecontrol.io/Package%20Control.sublime-package" -O ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package Control.sublime-package
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+git clone git@github.com:artushin/sublimePrefs.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
